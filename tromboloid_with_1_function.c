d5b256cdf016ca7c3f5286067324b0216c2208e4
harsha976 <75736543+harsha976@users.noreply.github.com>
2021-02-22 10:08:02 +0530

    Update tromboloid_with_1_function.c

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -4,7 +4,7 @@ int main()
   flote h,b,d;
   printf(“\Enter the value of h,b,and d\n”);
   scanf(“%f%f%f”,&h&b&d);
-  printf(“Volume of tromboloid is : %f\n”,((h*b*d)+(d/d))/3);
+  printf(“Volume of tromboloid is : %f\n”,((h*b*d)+(d/d))/3));
   return 0; 
 }
 
